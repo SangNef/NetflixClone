2275ab247ffe550a1b83745b77411a6cf8a40272
SangNef <sang.tv2614@gmail.com>
2023-05-24 14:25:11 +0700

    none

---
+++ src/pages/Login.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <img
+          className="hidden sm:block w-full h-full object-cover absolute"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/51e53f54-0d9f-40ec-9e05-c030def06ac9/59fd5bf8-0338-47a5-abb2-c78d169fcd8f/VN-vi-20230515-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
+          alt="/"
+        />
+        <div className="bg-black/60 fixed top-0 left-0 w-full h-screen"></div>
+        <div className="fixed w-full px-4 py-24 z-50">
+          <div className="max-w-[450px] h-[600px] mx-auto bg-black/75 text-white">
+            <div className="max-w-[320px] mx-auto py-16">
+              <h1 className="text-3xl font-bold">Log In</h1>
+              <form className="w-full flex flex-col py-4">
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="email"
+                  placeholder="Enter Your Email..."
+                />
+                <input
+                  className="p-3 my-2 bg-gray-700 rounded"
+                  type="password"
+                  placeholder="************"
+                />
+                <button className="bg-red-600 py-3 my-6 rounded font-bold">
+                  Log In
+                </button>
+                <div className="flex justify-between text-sm text-gray-600">
+                  <p>
+                    <input className="mr-2" type="checkbox" />
+                    Remember me
+                  </p>
+                  <p>Need Help?</p>
+                </div>
+                <p className="py-8">
+                  <span className="text-gray-600">
+                    Don't have Netflix account?
+                  </span>{" "}
+                  <Link to="/signup">Sign Up</Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
